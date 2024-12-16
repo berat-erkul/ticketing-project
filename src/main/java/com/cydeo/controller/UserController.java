d9914f2f460454b311d88cd8ab2bac26c655d91a
berat-erkul <beraterkul00@gmail.com>
2024-12-17 02:04:58 +0300

    I set delete button on /user/delete page

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -56,5 +56,9 @@ public class UserController {
         return "redirect:/user/create";
     }
 
-
+    @GetMapping("/delete/{username}")
+    public String deleteUser(@PathVariable("username") String username){
+        userService.deleteById(username);
+        return "redirect:/user/create";
+    }
 }
