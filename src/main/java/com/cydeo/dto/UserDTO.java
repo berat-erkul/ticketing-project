d19c5fdc487ac1abd6da20b94f17353d50f603fa
berat-erkul <beraterkul00@gmail.com>
2024-12-21 02:06:33 +0300

    I set validations for UserDTO.
    Then I added @Valid and BindingResult (interface) in UserController

--- src/main/java/com/cydeo/dto/UserDTO.java
+++ src/main/java/com/cydeo/dto/UserDTO.java
@@ -1,6 +1,7 @@
 package com.cydeo.dto;
 
 import com.cydeo.enums.Gender;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,11 +10,31 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDTO {
+
+    @NotBlank
+    @Size(max = 15, min = 3)
     private String firstName;
+
+    @NotBlank
+    @Size(max = 15, min = 3)
     private String lastName;
+
+    @NotBlank
+    @Email
     private String userName; //key of the object (keep it unique)
+
+    @NotNull
     private Gender gender;
+
+    @NotBlank
+    @Pattern(regexp = "\\d{3}-\\d{3}-\\d{2}-\\d{2}")
     private String phone;
-    private String passWord;
+
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$",
+            message = "Password must be at least 8 characters long, include uppercase, lowercase, a digit, and a special character.")
+    private String password;
+
+    @NotNull
     private RoleDTO role;  //Long id , String description
 }
